87197e91a6e25c3da4aee9f23e5fb3f6ad375cf9
Ali Raza <coder.raza@gmail.com>
2024-12-13 16:39:44 +0500

    changes

--- src/components/chatbot/chatbot.jsx
+++ src/components/chatbot/chatbot.jsx
@@ -2,10 +2,52 @@ import React, { useState, useEffect } from "react";
 import "./chatbot.css";
 import axios from 'axios'
 
-
 import PImage from '../../Images/pen_12332869.png';
 import iconImage from '../../Images/chatbot_6231457.png';
 import SentImage from '../../Images/paper-plane_3917620.png';
+import BoxIMage1 from '../../Images/box-image1.jpg'
+import BoxIMage2 from '../../Images/box-image2.png'
+
+
+function AmazingBoxes({ai_response_data}) {
+
+  const [boxesData, setBoxesData] = useState([])
+
+  useEffect(() => {
+    setBoxesData(ai_response_data);
+  })
+
+  return (
+    <div className="amazing-boxes">
+
+      {/* {boxesData.length} */}
+
+      <div className="bigger-box">
+        <img src={BoxIMage2} height={'360px'} width={'300px'} alt="" />
+        <p className="title-box">
+          AVA - AI Responder
+        </p>
+      </div>
+
+      <div className="boxes-set">
+        <div className="single-box">
+          <img height={'160px'} width={'300px'}src={BoxIMage1} alt="" />
+          <p className="title-box">
+            AVA - AI Menu Mixer
+          </p>
+        </div>
+
+        <div className="single-box">
+          <img height={'160px'} width={'300px'} src={BoxIMage1} alt="" />
+          <p className="title-box">
+            AVA - AI Sharing
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 
 const ChatBot = () => {
@@ -13,7 +55,7 @@ const ChatBot = () => {
   const [activeTab, setActiveTab] = useState("home");
   const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState("");
-  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(true);
 
   const [inputValue, setInputValue] = useState("");
   const [showResponse, setShowResponse] = useState(false); 
@@ -27,13 +69,13 @@ const ChatBot = () => {
   const handleSendClick = () => {
     if (inputValue.trim() !== "") { 
 
-      axios.get(`http://16.171.169.228:8000/agent/chat/?id=1&user_input=${inputValue}`)
+      axios.get(`http://localhost:8000/agent/chat/?id=1&user_input=${inputValue}`)
       .then((res) => {
         console.log(res.data.response.msg);
 
         setShowResponse(true); 
         setDisplayedResponse(''); 
-        simulateTyping(res.data.response.msg); // Start typing animation
+        simulateTyping(res.data.response.msg);
       })
       .catch(err => {
         console.log(err);
@@ -57,20 +99,19 @@ const ChatBot = () => {
   
     // Typing simulation
     const typingInterval = setInterval(() => {
-      console.log("Current index:", index); // Debugging the index
-      console.log("Current char:", msg[index]); // Debugging the current character being appended
-  
-      // Ensure msg[index] is valid and within bounds
+      console.log("Current index:", index); 
+      console.log("Current char:", msg[index]);
+      
       if (index < msg.length) {
         setDisplayedResponse((prev = "") => {
-          // Make sure we're appending valid characters, and avoid 'undefined' in the previous state
+          
           return (prev || "") + (msg[index] || ""); 
         });
-        index++; // Increment the index after appending the current character
+        index++; 
       } else {
-        clearInterval(typingInterval); // Stop the interval when done
+        clearInterval(typingInterval);
       }
-    }, 30);  // Adjust the interval time to fit the desired typing speed
+    }, 30);  
   };
   
   
@@ -78,9 +119,6 @@ const ChatBot = () => {
 
 
   useEffect(() => {
-
-
-    
     if (chatOpen && messages.length === 0) {
       const initialMessage = {
         text: "Hi, I am Mixeat, I would love to help your today's food selection.",
@@ -111,41 +149,25 @@ const ChatBot = () => {
         <div className={`chat-window ${isFullScreen ? "fullscreen" : ""}`}>
           <div className="chat-header">
             <div className="header-icons">
-              <img
-                className="expand-icon"
-                onClick={toggleFullScreen}
-                src="https://img.icons8.com/ios-glyphs/30/ffffff/full-screen.png"
-                alt="Expand"
-                style={{ cursor: "pointer" }}
-              />
-              <img
-                className="close-icon"
-                onClick={toggleChat}
-                src="https://img.icons8.com/ios-filled/30/ffffff/delete-sign.png"
-                alt="Close"
-                style={{ cursor: "pointer", marginLeft: "10px" }}
-              />
+              <div className="chat-header-buttons">
+                <button onClick={toggleChat} className="chat-sign-up-btn">Close</button>
+                <button className="chat-sign-up-btn">Sign Up</button>
+              </div>
             </div>
           </div>
 
-
           <div className="container">
           {!showResponse ? (
-            <div className="icon-box">
-              <img className="icon" src={iconImage} alt="Chatbot Icon" />
-              <h2>IAIA</h2>
-              <p>
-                YOUR ADVANCE VA.
-              </p>
+            <div className={"ai-box-response"} >
+              <AmazingBoxes />
             </div>
           ) : (
-            <div className={`${isFullScreen ? "dummy-response" : "dummy-response-mobile"}`} >
-              <p>{displayedResponse}</p>
+            <div className={"ai-response-container"} >
+              <p className="text-based-ai-response">{displayedResponse}</p>
             </div>
           )}
 
-
-          <div className={`${isFullScreen ? "suggestions-box" : "hide"}`}>
+          {/* <div className={`${isFullScreen ? "suggestions-box" : "hide"}`}>
             <h2>Prompt Suggestions for You</h2>
             <div className="suggestion-items">
               <SuggestionItem text="Lorem, ipsum dolor sit amet consectetur." onClick={handleSuggestionClick} />
@@ -153,22 +175,24 @@ const ChatBot = () => {
               <SuggestionItem text="Lorem ipsum dolor." onClick={handleSuggestionClick} />
               <SuggestionItem text="Lorem, ipsum dolor sit amet consectetur." onClick={handleSuggestionClick} />
             </div>
-          </div>
+          </div> */}
 
-          <div className="input-box" >
-              <input
-                className="input"
-                placeholder="Type your message..."
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-              <button className={isFullScreen ? "send-btn-opened" : 'send-btn'} > {isFullScreen ? "Back to Results" : "Back"}</button>
-              <div className="input-image" onClick={handleSendClick}>
-                <img src={SentImage} className="send-icon" alt="Send Icon" />
+          <div className="input-holder-chat">
+            <div className="input-box" >
+                <input
+                  className="input"
+                  placeholder="Type your message..."
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <div className="input-image" onClick={handleSendClick}>
+                  <img src={SentImage} className="send-icon" alt="Send Icon" />
+                </div>
               </div>
             </div>
           </div>
+
         </div>
       )}
     </div>
